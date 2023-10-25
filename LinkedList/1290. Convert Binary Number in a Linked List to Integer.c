efe2c14be370124c7310161fde24fb842974db87
oliviawu77 <jr5566i@yahoo.com.tw>
2023-10-25 17:31:39 +0800

    Update 1290. Convert Binary Number in a Linked List to Integer.c

--- LinkedList/1290. Convert Binary Number in a Linked List to Integer.c
+++ LinkedList/1290. Convert Binary Number in a Linked List to Integer.c
@@ -8,29 +8,12 @@
 typedef struct ListNode Node;
 
 int getDecimalValue(Node* head){
-    Node* cur;
-    Node* pre;
-    Node* tail;
-    int count = 0;
+    Node* cur = head;
+
     int dec_value = 0;
     
-    cur = head;
-
-    tail = cur->next;
-    cur->next = NULL;
-    pre = cur;
-    cur = tail;
-
-    while(tail){
-        tail = tail->next;
-        cur->next = pre;
-        pre = cur;
-        cur = tail;
-    }
-    cur = pre;
     while(cur){
-        dec_value += cur->val * pow(2, count);
-        count++;
+        dec_value = dec_value * 2 + cur->val;
         cur = cur->next;
     }
     return dec_value;
